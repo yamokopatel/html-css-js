e574c42b0ec68e83d760097c8c6d9f932c34f389
Deniss Cvetkovs <cvetkovsdeniss4@gmail.com>
2025-09-04 11:40:36 +0300

    Tried to add data listener for form.html

--- app.js
+++ app.js
@@ -9,3 +9,14 @@ function printNumbers(){
 function deleteParagraph(){
     document.querySelector("#first-paragraph").remove();
 }
+function loadLogData(){
+    document.getElementById("form").addEventListener(onkeydown("Enter"),logData())
+}
+function logData(){
+    console.log(document.getElementById("name").innerText);
+    console.log(document.getElementsByName("gender"));
+    console.log(document.getElementById("email").innerText);
+    console.log(document.getElementById("level").innerText);
+    console.log(document.getElementById("feedback").innerText);
+    console.log(document.getElementById("emailShare").innerText);
+}
